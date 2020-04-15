885f574c23e7fd5785a46b50e2f04a4b17726571
Braden Batman <braden.batman@gmail.com>
2020-04-14 22:35:35 -0400

    update tables

--- Useful Files/createtables.sql
+++ Useful Files/createtables.sql
@@ -6,14 +6,14 @@ CREATE TABLE "question"
  "incorrect1" varchar(255) NOT NULL,
  "incorrect2" varchar(255) NOT NULL,
  "incorrect3" varchar(255) NOT NULL,
- CONSTRAINT "PK_question" PRIMARY KEY ( "qID" )
+ CONSTRAINT "PK_question" PRIMARY KEY ( "qid" )
 );
 
 CREATE TABLE "student"
 (
  "name"      varchar(50) NOT NULL,
  "studentid" serial NOT NULL,
- CONSTRAINT "PK_student" PRIMARY KEY ( "studentID" )
+ CONSTRAINT "PK_student" PRIMARY KEY ( "studentid" )
 );
 
 CREATE TABLE "test"
@@ -23,20 +23,20 @@ CREATE TABLE "test"
  "question2" integer NOT NULL,
  "question3" integer NOT NULL,
  "question4" integer NOT NULL,
- CONSTRAINT "PK_test" PRIMARY KEY ( "testID" ),
- CONSTRAINT "FK_q1" FOREIGN KEY ( "question1" ) REFERENCES "question" ( "qID" ),
- CONSTRAINT "FK_q2" FOREIGN KEY ( "question2" ) REFERENCES "question" ( "qID" ),
- CONSTRAINT "FK_q3" FOREIGN KEY ( "question3" ) REFERENCES "question" ( "qID" ),
- CONSTRAINT "FK_q4" FOREIGN KEY ( "question4" ) REFERENCES "question" ( "qID" )
+ CONSTRAINT "PK_test" PRIMARY KEY ( "testid" ),
+ CONSTRAINT "FK_q1" FOREIGN KEY ( "question1" ) REFERENCES "question" ( "qid" ),
+ CONSTRAINT "FK_q2" FOREIGN KEY ( "question2" ) REFERENCES "question" ( "qid" ),
+ CONSTRAINT "FK_q3" FOREIGN KEY ( "question3" ) REFERENCES "question" ( "qid" ),
+ CONSTRAINT "FK_q4" FOREIGN KEY ( "question4" ) REFERENCES "question" ( "qid" )
 );
 
 CREATE TABLE "testlog"
 (
  "logid"      serial NOT NULL,
- "numCorrect" integer NOT NULL,
+ "numcorrect" integer NOT NULL,
  "studentid"  integer NOT NULL,
  "testid"     integer NOT NULL,
- CONSTRAINT "PK_testlog" PRIMARY KEY ( "logID" ),
- CONSTRAINT "FK_studentID" FOREIGN KEY ( "studentID" ) REFERENCES "student" ( "studentID" ),
- CONSTRAINT "FK_testID" FOREIGN KEY ( "testID" ) REFERENCES "test" ( "testID" )
+ CONSTRAINT "PK_testlog" PRIMARY KEY ( "logid" ),
+ CONSTRAINT "FK_studentID" FOREIGN KEY ( "studentid" ) REFERENCES "student" ( "studentid" ),
+ CONSTRAINT "FK_testID" FOREIGN KEY ( "testid" ) REFERENCES "test" ( "testid" )
 );
